6bf57b26c227b927d1457ddf882ce982527922fa
Sonu Poonia <sonu.poonia@algoshack.com>
2024-05-14 15:38:16 +0530

    setup format

--- InstaDSA.js
+++ InstaDSA.js
@@ -1,33 +1,44 @@
 // Welcome Buddy, Let's Code
 
 const Information = {
-    By: 'Sonu_The_Developer',
-    Date: '14-May-2024',
-    Place: 'Bangalore, Karnataka',
-    Contact: {
-        whatsApp: 'https://wa.me/+918572880368',
-        email: 'sonupoonia85728@gmail.com'
-    },
-    SocialMedia: {
-        instagram: 'https://instagram.com/Sonu_The_Developer',
-        Twitter: 'https://twitter.com/S_The_Developer',
-        Github: 'Sonu-The-Developer',
-    },
-    Challenge: 'InstaDSA',
-    CreditTo: {
-        Instagram: 'https://instagram.com/hiten.codes',
-        Twitter: 'https://twitter.com/hiten_codes',
-    }
-}
+  By: "Sonu_The_Developer",
+  Date: "14-May-2024",
+  Place: "Bangalore, Karnataka",
+  Contact: {
+    WhatsApp: "https://wa.me/+918572880368",
+    Email: "sonupoonia85728@gmail.com",
+  },
+  SocialMedia: {
+    Instagram: "https://instagram.com/Sonu_The_Developer",
+    Twitter: "https://twitter.com/S_The_Developer",
+    Github: "Sonu-The-Developer",
+  },
+  Challenge: "InstaDSA",
+  Language: "javascript",
+  CreditTo: {
+    Instagram: "https://instagram.com/hiten.codes",
+    Twitter: "https://twitter.com/hiten_codes",
+  },
+  SolvedBy: {
+    Instagram: "https://instagram.com/Sonu_The_Developer",
+    Twitter: "https://twitter.com/S_The_Developer",
+    Github: "https://github.com/Sonu-The-Developer",
+  },
+};
 
-// ? InstaDSA Problem 01
-const Problem01 = {
-    Challenge: 'InstaDSA',
-    language: 'javascript',
-    url: 'https://leetcode.com/problems/palindrome-number/description',
-    solvedBy: {
-        instagram: 'https://instagram.com/Sonu_The_Developer',
-        Twitter: 'https://twitter.com/S_The_Developer',
-        Github: 'https://github.com/Sonu-The-Developer',
-    }
+// ? InstaDSA Problem 01 - Palindrome Number
+{
+  const Problem = {
+    Problem: "Palindrome Number",
+    Url: "https://leetcode.com/problems/palindrome-number/description",
+    ...Information?.SolvedBy,
+    ...Information?.Challenge,
+    ...Information?.Language,
+  };
+
+  // ? Solution
+  const Solution = () => {};
+
+  console.log(`\n *** Output Of ${Problem?.Problem} *** \n`);
+  console.log(`${Solution(10)} \n`);
 }
