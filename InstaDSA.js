52af51c8961bfb17fcfb015df313b3d53049456e
Sonu Poonia <sonu.poonia@algoshack.com>
2024-05-14 16:29:02 +0530

    setup

--- InstaDSA.js
+++ InstaDSA.js
@@ -1,31 +1,6 @@
 // Welcome Buddy, Let's Code
 
-const Information = {
-  By: "Sonu_The_Developer",
-  Date: "14-May-2024",
-  Place: "Bangalore, Karnataka",
-  Contact: {
-    WhatsApp: "https://wa.me/+918572880368",
-    Email: "sonupoonia85728@gmail.com",
-  },
-  SocialMedia: {
-    Instagram: "https://instagram.com/Sonu_The_Developer",
-    Twitter: "https://twitter.com/S_The_Developer",
-    Github: "Sonu-The-Developer",
-  },
-  Challenge: "InstaDSA",
-  Language: "javascript",
-  CreditTo: {
-    Instagram: "https://instagram.com/hiten.codes",
-    Twitter: "https://twitter.com/hiten_codes",
-  },
-  SolvedBy: {
-    Instagram: "https://instagram.com/Sonu_The_Developer",
-    Twitter: "https://twitter.com/S_The_Developer",
-    Github: "https://github.com/Sonu-The-Developer",
-  },
-  FollowLinkFormat: "   ### **- [Go To The Problem URL](link)**   ",
-};
+import Information from "./Information";
 
 // ? InstaDSA Problem 01 - Palindrome Number
 {
@@ -38,8 +13,8 @@ const Information = {
   };
 
   // ? Solution
-  const Solution = () => {};
+  const Solution = (n) => {};
 
   console.log(`\n *** Output Of ${Problem?.Problem} *** \n`);
-  console.log(`${Solution(10)} \n`);
+  console.log(`Solution(10) : ${Solution(10)} \n`);
 }
