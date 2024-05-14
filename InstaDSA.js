05f6e5f723102c7c097f95c1980c312bd3853298
Sonu Poonia <sonu.poonia@algoshack.com>
2024-05-14 17:22:58 +0530

    setup

--- InstaDSA.js
+++ InstaDSA.js
@@ -1,6 +1,31 @@
 // Welcome Buddy, Let's Code
 
-import Information from "./Information";
+const Information = {
+  By: "Sonu_The_Developer",
+  Date: "14-May-2024",
+  Place: "Bangalore, Karnataka",
+  Contact: {
+    WhatsApp: "https://wa.me/+918572880368",
+    Email: "sonupoonia85728@gmail.com",
+  },
+  SocialMedia: {
+    Instagram: "https://instagram.com/Sonu_The_Developer",
+    Twitter: "https://twitter.com/S_The_Developer",
+    Github: "Sonu-The-Developer",
+  },
+  Challenge: "InstaDSA",
+  Language: "javascript",
+  CreditTo: {
+    Instagram: "https://instagram.com/hiten.codes",
+    Twitter: "https://twitter.com/hiten_codes",
+  },
+  SolvedBy: {
+    Instagram: "https://instagram.com/Sonu_The_Developer",
+    Twitter: "https://twitter.com/S_The_Developer",
+    Github: "https://github.com/Sonu-The-Developer",
+  },
+  FollowLinkFormat: "   ### **- [Go To The Problem URL](link)**   ",
+};
 
 // ? InstaDSA Problem 01 - Palindrome Number
 {
@@ -15,6 +40,6 @@ import Information from "./Information";
   // ? Solution
   const Solution = (n) => {};
 
-  console.log(`\n *** Output Of ${Problem?.Problem} *** \n`);
+  console.log(`\n*** Output Of ${Problem?.Problem} ***`);
   console.log(`Solution(10) : ${Solution(10)} \n`);
 }
