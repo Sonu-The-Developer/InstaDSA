67a6a0181a06d4e724c590c1e5d3beb23248c99e
Sonu Poonia <sonu.poonia@algoshack.com>
2024-05-14 15:13:40 +0530

    first commit

---
+++ InstaDSA.js
@@ -0,0 +1,33 @@
+// Welcome Buddy, Let's Code
+
+const Information = {
+    By: 'Sonu_The_Developer',
+    Date: '14-May-2024',
+    Place: 'Bangalore, Karnataka',
+    Contact: {
+        whatsApp: 'https://wa.me/+918572880368',
+        email: 'sonupoonia85728@gmail.com'
+    },
+    SocialMedia: {
+        instagram: 'https://instagram.com/Sonu_The_Developer',
+        Twitter: 'https://twitter.com/S_The_Developer',
+        Github: 'Sonu-The-Developer',
+    },
+    Challenge: 'InstaDSA',
+    CreditTo: {
+        Instagram: 'https://instagram.com/hiten.codes',
+        Twitter: 'https://twitter.com/hiten_codes',
+    }
+}
+
+// ? InstaDSA Problem 01
+const Problem01 = {
+    Challenge: 'InstaDSA',
+    language: 'javascript',
+    url: 'https://leetcode.com/problems/palindrome-number/description',
+    solvedBy: {
+        instagram: 'https://instagram.com/Sonu_The_Developer',
+        Twitter: 'https://twitter.com/S_The_Developer',
+        Github: 'https://github.com/Sonu-The-Developer',
+    }
+}
